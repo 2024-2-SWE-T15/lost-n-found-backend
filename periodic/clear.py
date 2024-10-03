36d6346fc322a48530512fc20c2d4d4e59b7c828
JuhyunKim <juhy0987@naver.com>
2024-10-04 03:11:35 +0900

    Add API routers for post_func and coordinates_func

--- periodic/clear.py
+++ periodic/clear.py
@@ -1,9 +1,14 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 
+from db.mysql import database as mysql_db
+from db.mysql import model as mysql_model
+from db.mysql import crud as mysql_crud
+
+from db.sqlite import database as sqlite_db
 from db.sqlite import model as sqlite_model
 from db.sqlite import crud as sqlite_crud
-from db.sqlite import database as sqlite_db
+
 
 def regularClear():
   SessionLocal = sqlite_db.initDB()
@@ -13,3 +18,6 @@ def regularClear():
   finally:
     db.close()
   
+def suggestion():
+  
+  pass
