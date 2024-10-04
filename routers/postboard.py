063bd63ad0d0f481fd25538ff6c604eaf5b739be
JuhyunKim <juhy0987@naver.com>
2024-10-04 17:42:32 +0900

    Refactor registerPost function in postboard.py to improve post registration process

--- routers/postboard.py
+++ routers/postboard.py
@@ -42,13 +42,13 @@ async def registerPost(postSchemaAdd: mysql_schema.PostSchemaAdd,
                                                                 user_id=user.id, 
                                                                 coordinates=tuple2PointString(postSchemaAdd.coordinates), 
                                                                 description=postSchemaAdd.description,
-                                                                is_lost=False))):
+                                                                is_lost=postSchemaAdd.is_lost))):
     raise HTTPException(status_code=500, detail='Failed to register post')
   
   # if found, register kept location
   if not postSchemaAdd.is_lost:
     if not (kept := mysql_crud.kept.register(db, mysql_model.Kept(post_id=post.id, 
-                                                              coordinates=postSchemaAdd.coordinates, 
+                                                              coordinates=tuple2PointString(postSchemaAdd.kept_coordinates), 
                                                               stronghold_id=postSchemaAdd.stronghold_id))):
       raise HTTPException(status_code=500, detail='Failed to register kept')
   
@@ -70,11 +70,12 @@ async def registerPost(postSchemaAdd: mysql_schema.PostSchemaAdd,
       pass
     
   # personal_idlist
-  for name, value in postSchemaAdd.personal_idlist.items():
-    if not (identity := mysql_crud.identity.register(db, mysql_model.Identity(post_id=post.id, 
-                                                                              name=name,
-                                                                              value=value))):
-      pass
+  if postSchemaAdd.personal_idlist:
+    for name, value in postSchemaAdd.personal_idlist.items():
+      if not (identity := mysql_crud.identity.register(db, mysql_model.Identity(post_id=post.id, 
+                                                                                name=name,
+                                                                                value=value))):
+        pass
   
   postView = mysql_crud.view.get(db, post.id)
   return point2TupleStructure(postView)
