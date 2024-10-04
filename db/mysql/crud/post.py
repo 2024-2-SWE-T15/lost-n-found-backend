0c7e95a7935e0f7223930ffd62a892914ad06f45
JuhyunKim <juhy0987@naver.com>
2024-10-04 14:37:20 +0900

    Refactor get function in post.py to support filtering by postSchema fields

--- db/mysql/crud/post.py
+++ db/mysql/crud/post.py
@@ -7,9 +7,20 @@ from ..model import Post
 from ..schema import PostSchema
 
 
-def get(db: Session, post_id: str):
-  db_item = db.query(Post).filter(Post.id == post_id).first()
+def get(db: Session, postSchema: PostSchema):
+  query = db.query(Post)
+  if postSchema.id:
+    query = query.filter(Post.id == postSchema.id)
+  if postSchema.user_id:
+    query = query.filter(Post.user_id == postSchema.user_id)
+  if postSchema.valid is not None:
+    query = query.filter(Post.valid == postSchema.valid)
+  if postSchema.is_lost:
+    query = query.filter(Post.is_lost == postSchema.is_lost)
+  
+  db_item = query.first()
   return db_item
+
 
 def register(db: Session, post: PostSchema):
   db_item = Post(
