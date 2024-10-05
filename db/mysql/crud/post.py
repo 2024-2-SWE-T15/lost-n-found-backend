4b9f4185779b4cf51803e9ba6d12c6f10f8fa239
JuhyunKim <juhy0987@naver.com>
2024-10-06 02:24:42 +0900

    Refactor: Update get function in post.py to accept post_id instead of postSchema

--- db/mysql/crud/post.py
+++ db/mysql/crud/post.py
@@ -11,18 +11,8 @@ from ..schema import PostSchema
 
 load_dotenv('config/.env')
 
-def get(db: Session, postSchema: PostSchema):
-  query = db.query(Post)
-  if postSchema.id:
-    query = query.filter(Post.id == postSchema.id)
-  if postSchema.user_id:
-    query = query.filter(Post.user_id == postSchema.user_id)
-  if postSchema.valid is not None:
-    query = query.filter(Post.valid == postSchema.valid)
-  if postSchema.is_lost:
-    query = query.filter(Post.is_lost == postSchema.is_lost)
-  
-  db_item = query.first()
+def get(db: Session, post_id: str):
+  db_item = db.query(Post).filter(Post.id == post_id).first()
   return db_item
 
 
