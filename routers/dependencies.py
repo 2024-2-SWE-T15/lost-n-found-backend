7065ab5f2c7d597460bf21fb1ae2cdd652b4454f
JuhyunKim <juhy0987@naver.com>
2024-10-06 02:19:27 +0900

    Refactor: Update oauth authorization prompt in dependencies.py

--- routers/dependencies.py
+++ routers/dependencies.py
@@ -34,10 +34,10 @@ oauth.register(
   authorize_url='https://accounts.google.com/o/oauth2/auth',
   authorize_params={
     'access_type': 'offline',
+    'prompt': 'consent', # authorization prompt : request user consent every time, if not set, it requires refresh token
   },
   access_token_url='https://accounts.google.com/o/oauth2/token',
   access_token_params={
-    'grant_type': 'refresh_token',
     'access_type': 'offline',
   },
   refresh_token_url=None,
