961d793520a49a96b8db9e0263e246e11f0a55db
JuhyunKim <juhy0987@naver.com>
2024-10-09 14:30:02 +0900

    Refactor: Add image router to post_func/__init__.py

--- routers/post_func/__init__.py
+++ routers/post_func/__init__.py
@@ -3,13 +3,15 @@ from fastapi import APIRouter
 from . import search
 from . import personal_id
 from . import hashtag
+from . import image
 
 router = APIRouter()
 
 router.include_router(search.router)
 
 
-post_router = APIRouter(prefix='/{post_id}')
+post_router = APIRouter(prefix='/{post_id}', tags=[])
 
 post_router.include_router(personal_id.router)
 post_router.include_router(hashtag.router)
+post_router.include_router(image.router)
